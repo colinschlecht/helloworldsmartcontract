050c9637f9c1abfebf993e4619295de5e09a4245
Colin Schlecht <schlechtcolin@yahoo.com>
2021-05-21 13:14:59 -0700

    merges client

--- client/src/util/interact.js
+++ client/src/util/interact.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(alchemyKey); 
-const contractABI = require("../contract-abi.json")
+const web3 = createAlchemyWeb3(alchemyKey);
+console.log(alchemyKey)
+
+const contractABI = require("../contract-abi.json");
 const contractAddress = "0x6f3f635A9762B47954229Ea479b4541eAF402A6A";
-​
+
 export const helloWorldContract = new web3.eth.Contract(
   contractABI,
   contractAddress
